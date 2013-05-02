7f646060549b979c288182a985abce1a63021978
Aldrin <drinmontana@gmail.com>
2013-05-02 10:32:35 -0700

    Made changes to calculatePyroSimilarities to remove NULLs (if a float is null then any arithmetic operation including that operand will result in NULL)
    and added a lot of commented out tables to createPyroSimTables that were used
    for debugging

--- sql/calculatePyroSimilarities.sql
+++ sql/calculatePyroSimilarities.sql
@@ -10,52 +10,58 @@ BEGIN
    DECLARE ISOLATE_SELECT CURSOR FOR
       SELECT i1.test_isolate_id, i1.name_prefix, i1.name_suffix,
              i2.test_isolate_id, i2.name_prefix, i2.name_suffix
-      FROM test_isolates i1 join test_isolates i2 ON (
-              i2.test_isolate_id > i1.test_isolate_id
-           )
-      WHERE i2.test_isolate_id > i1.test_isolate_id;
+      FROM test_isolates i1 join test_isolates i2 ON (i2.test_isolate_id > i1.test_isolate_id)
+      WHERE i1.test_isolate_id not in (select distinct test_isolate_id_1 from iso_sims)
+      LIMIT 1000;
    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
    OPEN ISOLATE_SELECT;
 
    SELECTION_LOOP: LOOP
-      FETCH ISOLATE_SELECT INTO (iso_id_1, iso_prefix_1, iso_suffix_1,
-                                 iso_id_2, iso_prefix_2, iso_suffix_2);
+      FETCH ISOLATE_SELECT INTO iso_id_1, iso_prefix_1, iso_suffix_1,
+                                iso_id_2, iso_prefix_2, iso_suffix_2;
 
-      compareIsos(iso_id_1, iso_id_2,
-                  iso_prefix_1, iso_suffix_1,
-                  iso_prefix_2, iso_suffix_2);
+      CALL compareIsos(iso_id_1, iso_id_2,
+                       iso_prefix_1, iso_suffix_1,
+                       iso_prefix_2, iso_suffix_2);
 
       IF done THEN
          LEAVE SELECTION_LOOP;
       END IF;
    END LOOP;
-END
+END$$
 
 DROP PROCEDURE IF EXISTS compareIsos$$
 CREATE PROCEDURE compareIsos(IN isolate_id_1 INT, IN isolate_id_2 INT,
                              IN iso_desig_1 VARCHAR(15), IN iso_id_1 INT,
                              IN iso_desig_2 VARCHAR(15), IN iso_id_2 INT)
 BEGIN
-   DECLARE region_sim_1, region_sim_2 FLOAT DEFAULT 0;
-   DECLARE iso_sim FLOAT;
+   DECLARE region_sim_1, region_sim_2 DEC(5, 4) DEFAULT 0;
+   DECLARE iso_sim DEC(5, 4) DEFAULT 0;
+   DECLARE reg_1_protocol, reg_2_protocol TINYINT DEFAULT 1;
 
-   compareRegion(iso_desig_1, iso_id_1, iso_desig_2, iso_id_2, '16-23', region_sim_1);
-   compareRegion(iso_desig_1, iso_id_1, iso_desig_2, iso_id_2, '23-5', region_sim_2);
+   CALL compareRegion(iso_desig_1, iso_id_1, iso_desig_2, iso_id_2, '16-23', region_sim_1);
+   CALL compareRegion(iso_desig_1, iso_id_1, iso_desig_2, iso_id_2, '23-5', region_sim_2);
 
-   SET iso_sim = (region_sim_1 + region_sim_2) / 2;
+   IF region_sim_1 < 0 OR region_sim_2 < 0 THEN
+      SET iso_sim = -2;
+   ELSE
+      SET iso_sim = (region_sim_1 + region_sim_2) / 2;
+   END IF;
 
    REPLACE INTO iso_sims(test_isolate_id_1, test_isolate_id_2, similarity)
                 VALUES (isolate_id_1, isolate_id_2, iso_sim);
 END$$
 
-DROP PROCEDIRE IF EXISTS compareRegion$$
-CREATE PROCEDURE compareRegion(IN iso_desig_1 VARCHAR(15), IN iso_id_1 INT
+DROP PROCEDURE IF EXISTS compareRegion$$
+CREATE PROCEDURE compareRegion(IN iso_desig_1 VARCHAR(15), IN iso_id_1 INT,
                                IN iso_desig_2 VARCHAR(15), IN iso_id_2 INT,
-                               IN region VARCHAR(20), OUT region_sim FLOAT)
+                               IN region VARCHAR(20), OUT region_sim DEC(5, 4))
 BEGIN
-   DECLARE done, pyro_id_1, pyro_id_2, compare_count INT DEFAULT 0;
-   DECLARE pyro_sim, total_sim DEC(8, 4) DEFAULT 0;
+   DECLARE done, pyro_id_1, pyro_id_2, compare_count, pyro_len INT DEFAULT 0;
+   DECLARE pyro_sim DEC(5, 4) DEFAULT 0;
+   DECLARE total_sim FLOAT DEFAULT 0;
+   DECLARE pyro_protocol TINYINT DEFAULT 1;
 
    DECLARE COMPARE_CURSOR CURSOR FOR
       SELECT p1.pyroID, p2.pyroID
@@ -64,20 +70,29 @@ BEGIN
               p2.name_prefix = iso_desig_2 AND p2.name_suffix = iso_id_2 AND
               p1.appliedRegion = region AND p2.appliedRegion = region AND
               p1.dsName = p2.dsName AND p1.forPrimer = p2.forPrimer AND
-              p1.revPrimer = p2.revPrimer AND p1.seqPrimer = p2.seqPrimer AND
-              p2.pyroID > p1.pyroID
+              p1.revPrimer = p2.revPrimer AND p1.seqPrimer = p2.seqPrimer
            );
    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
    OPEN COMPARE_CURSOR;
 
    PYRO_COMPARE_LOOP: LOOP
-      FETCH COMPARE_CURSOR INTO (pyro_id_1, pyro_id_2);
+      FETCH COMPARE_CURSOR INTO pyro_id_1, pyro_id_2;
 
-      comparePyros(pyro_id_1, pyro_id_2, pyro_sim);
+      IF region = '16-23' THEN
+         SET pyro_len = 95;
+      ELSE
+         IF region = '23-5' THEN
+            SET pyro_len = 93;
+         END IF;
+      END IF;
 
-      SET total_sim = total_sim + pyro_sim;
-      SET compare_count = compare_count + 1;
+      CALL comparePyros(pyro_id_1, pyro_id_2, pyro_len, pyro_sim);
+
+      IF pyro_sim IS NOT NULL THEN
+         SET total_sim = total_sim + pyro_sim;
+         SET compare_count = compare_count + 1;
+      END IF;
 
       IF done THEN
          LEAVE PYRO_COMPARE_LOOP;
@@ -85,38 +100,40 @@ BEGIN
 
    END LOOP;
 
-   SET region_sim = total_sim / compare_count;
+   IF total_sim IS NOT NULL AND compare_count > 0 THEN
+      SET region_sim = total_sim / compare_count;
+   ELSE
+      SET region_sim = -1;
+   END IF;
 END$$
 
 DROP PROCEDURE IF EXISTS comparePyros$$
-CREATE PROCEDURE comparePyros(IN pyro_id_1 INT, IN pyro_id_2 INT, OUT pyro_sim DEC(8, 4) DEFAULT 0)
+CREATE PROCEDURE comparePyros(IN pyro_id_1 INT, IN pyro_id_2 INT, IN pyro_len INT,
+                              OUT pyro_sim DEC(5, 4))
 BEGIN
-   DECLARE peak_count INT DEFAULT 0;
+   DECLARE peak_count, done INT DEFAULT 0;
    DECLARE pyro_1_sum, pyro_2_sum, pyro_product FLOAT DEFAULT 0;
    DECLARE pyro_1_squared_sum, pyro_2_squared_sum FLOAT DEFAULT 0;
-   DECLARE done, pos_1, pos_2 INT DEFAULT 0;
    DECLARE nucleotide_1, nucleotide_2 VARCHAR(1) DEFAULT NULL;
-   DECLARE peak_height_1, peak_height_2 DEC(8, 4) DEFAULT 0;
+   DECLARE peak_height_1, peak_height_2 DEC(5, 4) DEFAULT 0;
 
    DECLARE COMPARE_CURSOR CURSOR FOR
-      SELECT p1.position, p1.nucleotide, p1.pHeight,
-             p2.position, p2.nucleotide, p2.pHeight
-      FROM test_pyroprints p1 join test_histograms h1 on (
-              p1.pyroID = h1.pyroID AND p1.name_prefix = iso_desig_1 AND
-              p1.name_suffix = iso_id_1 AND p1.appliedRegion = region),
-           test_pyroprints p2 join test_histograms h2 on (
-              p2.pyroID = h2.pyroID AND p2.name_prefix = iso_desig_2 AND
-              p2.name_suffix = iso_id_2 AND p2.appliedRegion = region)
-      ORDER BY p1.pyroID, p1.position, p2.pyroID, p2.position;
+      SELECT h1.nucleotide, h1.pHeight, h2.nucleotide, h2.pHeight
+      FROM test_histograms h1 join test_histograms h2 on (
+              h1.pyroID = pyro_id_1 AND h2.pyroID = pyro_id_2 AND
+              h1.position = h2.position AND h1.position <= pyro_len AND
+              h2.position < pyro_len
+           )
+      ORDER BY h1.pyroID, h1.position, h2.pyroID, h2.position;
    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
    OPEN COMPARE_CURSOR;
 
    PYRO_COMPARE_LOOP: LOOP
-      FETCH COMPARE_CURSOR INTO (pos_1, nucleotide_1, peak_height_1,
-                                 pos_2, nucleotide_2, peak_height_2);
+      FETCH COMPARE_CURSOR INTO nucleotide_1, peak_height_1,
+                                nucleotide_2, peak_height_2;
 
-      IF pos_1 = pos_2 AND nucleotide_1 = nucleotide_2 THEN
+      IF nucleotide_1 = nucleotide_2 THEN
          SET peak_count = peak_count + 1;
 
          SET pyro_1_sum = pyro_1_sum + peak_height_1;
@@ -142,3 +159,10 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+
+-- REPLACE INTO pyro_sims VALUES (pyro_id_1, pyro_id_2, total_sim);
+-- REPLACE INTO tmp_vals(count) VALUES (compare_count);
+
+-- REPLACE INTO region_sims VALUES(isolate_id_1, isolate_id_2, pyro_id_1, pyro_id_2, region, total_sim);
+-- REPLACE INTO region_sims VALUES(isolate_id_1, isolate_id_2, region, pyro_sim);
--- sql/createPyroSimTables.sql
+++ sql/createPyroSimTables.sql
@@ -1,12 +1,38 @@
 DROP TABLE IF EXISTS iso_sims;
-
-CREATE TABLE IF NOT EXISTS iso_sims (
+CREATE TABLE iso_sims (
    iso_sim_id INT PRIMARY KEY AUTO_INCREMENT,
    test_isolate_id_1 INT,
    test_isolate_id_2 INT,
-   similarity DEC(4, 4),
+   similarity DEC(5, 4),
    UNIQUE (test_isolate_id_1, test_isolate_id_2),
    KEY isolate_index_1 (test_isolate_id_1),
    KEY isolate_index_2 (test_isolate_id_2),
    KEY similarity_index (similarity)
 );
+
+DROP TABLE IF EXISTS pyro_sims;
+-- CREATE TABLE pyro_sims (
+--    pyro_id_1 INT,
+  --  pyro_id_2 INT,
+  --  pyro_sim DEC(4, 4),
+  --  PRIMARY KEY(pyro_id_1, pyro_id_2),
+  --  KEY sim_index (pyro_sim)
+-- );
+
+DROP TABLE IF EXISTS region_sims;
+-- CREATE TABLE region_sims (
+  --  isolate_id_1 INT,
+  --  isolate_id_2 INT,
+  --  pyro_id_1 INT,
+  --  pyro_id_2 INT,
+  --  region_name VARCHAR(20),
+  --  region_sim DEC(4, 4),
+  --  PRIMARY KEY (isolate_id_1, isolate_id_2, pyro_id_1, pyro_id_2, region_name),
+  --  KEY region_index (region_name),
+  --  KEY sim_index (region_sim)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS tmp_vals (
+  --  tmp_val_id INT PRIMARY KEY AUTO_INCREMENT,
+  --  count INT
+-- );
