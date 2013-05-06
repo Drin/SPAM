738ada68733558ae2dd346f2d7d6468d77a2e5ba
Aldrin Montana <DrinMontana@gmail.com>
2013-05-05 18:47:36 -0700

    Changed the queries in CPLOP up a bit and now grabbing peak heights is way faster. Also set it up so that get_isolate_data returns a single array for each isolate that is the pyroprint for the 23-5 and 16-23 region

--- com/drin/python/similarity_generation/CPLOP.py
+++ com/drin/python/similarity_generation/CPLOP.py
@@ -1,33 +1,52 @@
 import os
 import sys
+import array
 import random
+
 import pymysql
 
+REGION_23_5 = 0
+REGION_16_23 = 1
+
+REGION_23_5_LEN = 93
+REGION_16_23_LEN = 95
+ISOLATE_LEN = 188
+
+ID_QUERY_OFF = 1
+DATA_QUERY_OFF = 2
+
 ID_QUERY = '''
-   SELECT test_isolate_id FROM test_isolates ORDER BY RAND(%d) LIMIT %d
+   SELECT distinct test_isolate_id, p1.pyroID, p2.pyroID
+   FROM test_isolates t
+        JOIN test_pyroprints p1 on (
+           t.name_prefix = p1.name_prefix AND
+           t.name_suffix = p1.name_suffix AND
+           p1.appliedRegion = '23-5'
+        )
+        JOIN test_pyroprints p2 on (
+           t.name_prefix = p2.name_prefix AND
+           t.name_suffix = p2.name_suffix AND
+           p2.appliedRegion = '16-23'
+        )
+   ORDER BY RAND(%d)
+   LIMIT %d
 '''
 
 DATA_QUERY = '''
-   SELECT test_isolate_id, h1.position, region1, region2,
-          h1.pHeight, h2.pHeight
-   FROM (SELECT test_isolate_id, p1.pyroID as pyro1, p2.pyroID as pyro2,
-                p1.appliedRegion as region1, p2.appliedRegion as region2
-         FROM test_isolates t
-              JOIN test_pyroprints p1 on (
-                      t.name_prefix = p1.name_prefix AND
-                      t.name_suffix = p1.name_suffix AND
-                      p1.appliedRegion = '23-5'
-                   )
-              JOIN test_pyroprints p2 on (
-                      t.name_prefix = p2.name_prefix AND
-                      t.name_suffix = p2.name_suffix AND
-                      p2.appliedRegion = '16-23'
-                   )
-         WHERE test_isolate_id in (%s)
-         GROUP BY t.name_prefix, t.name_suffix
-        ) pyros
-        JOIN test_histograms h1 on (h1.pyroID = pyro1)
-        JOIN test_histograms h2 on (h2.pyroID = pyro2)
+   SELECT p1.name_prefix, p1.name_suffix, h1.pHeight, h2.pHeight
+   FROM test_histograms h1
+        JOIN test_pyroprints p1 ON (
+           h1.pyroID = p1.pyroID AND
+           h1.position < 95 AND
+           p1.pyroID IN (%s)
+        )
+        JOIN test_histograms h2 USING (position)
+        JOIN test_pyroprints p2 on (
+           p1.name_prefix = p2.name_prefix AND
+           p1.name_suffix = p2.name_suffix AND
+           h2.pyroID = p2.pyroID AND
+           p2.pyroID IN (%s)
+        )
 '''
 
 class connection(object):
@@ -39,23 +58,50 @@ class connection(object):
                                                  user='drin', passwd='')
 
    def get_isolate_ids(self, db_seed=random.randrange(9999), limit=110000):
+      pyro_id_list_1 = array.array('L', (0,) * limit)
+      pyro_id_list_2 = array.array('L', (0,) * limit)
+
       cplop_cursor = self.CPLOP_CONNECTION.cursor()
       cplop_cursor.execute(ID_QUERY % (db_seed, limit))
-      results = [str(iso_tuple[0]) for iso_tuple in cplop_cursor.fetchall()]
-      cplop_cursor.close()
 
-      return results
+      data_ndx = 0
+      for iso_tuple in cplop_cursor.fetchall():
+         # attribute index is shifted by one because of test_isolate_id
+         pyro_id_list_1[data_ndx] = iso_tuple[REGION_23_5 + ID_QUERY_OFF]
+         pyro_id_list_2[data_ndx] = iso_tuple[REGION_16_23 + ID_QUERY_OFF]
+
+         data_ndx += 1
+
+      cplop_cursor.close()
+      return (pyro_id_list_1, pyro_id_list_2) 
 
    def get_isolate_data(self, data=None, limit=110000):
       if (data is None): return
 
+      isolate_data = dict()
       cplop_cursor = self.CPLOP_CONNECTION.cursor()
+      cplop_cursor.execute(DATA_QUERY % (
+         ','.join([str(val) for val in data[REGION_23_5]]),
+         ','.join([str(val) for val in data[REGION_16_23]])
+      ))
 
-      if ('DEBUG' in os.environ): print("querying isolate data...")
-      cplop_cursor.execute(DATA_QUERY % str(', '.join(data)))
-      if ('DEBUG' in os.environ): print("finished querying isolate data")
+      (isolate_id, peak_ndx) = (None, 0)
+      for data_tuple in cplop_cursor.fetchall():
+         tmp_isolate_id = "%s%s" % (data_tuple[0], str(data_tuple[1]))
 
-      results = cplop_cursor.fetchall()
+         if (tmp_isolate_id != isolate_id):
+            (isolate_id, peak_ndx) = (tmp_isolate_id, 0)
+            isolate_data[isolate_id] = array.array('f', (0,) * ISOLATE_LEN)
+
+         isolate_arr = isolate_data[isolate_id]
+
+         if (peak_ndx < REGION_23_5_LEN):
+            isolate_arr[peak_ndx] = data_tuple[REGION_23_5 + DATA_QUERY_OFF]
+
+         if (REGION_23_5_LEN + peak_ndx < ISOLATE_LEN):
+            isolate_arr[REGION_23_5_LEN + peak_ndx] = data_tuple[REGION_16_23 + DATA_QUERY_OFF]
+
+         peak_ndx += 1
+
       cplop_cursor.close()
-
-      return results
+      return isolate_data
--- com/drin/python/similarity_generation/OHClustEvaluation.py
+++ com/drin/python/similarity_generation/OHClustEvaluation.py
@@ -1,26 +1,32 @@
 import os
+import sys
+import time
+
 import CPLOP
 
 DEBUG_PEEK_SIZE = 10
 
-def debug(db_tuples):
-   for tuple_ndx in range(min(DEBUG_PEEK_SIZE, len(db_tuples))):
-      print("\t%s" % (db_tuples[tuple_ndx]))
-
 if (__name__ == '__main__'):
-   #(initialSize, updateSize, numUpdates) = (10000, 1000, 100)
-   (initialSize, updateSize, numUpdates) = (1000, 1000, 1)
+   (initialSize, updateSize, numUpdates) = (10000, 1000, 100)
    dataset_size = initialSize + (updateSize * numUpdates)
 
    conn = CPLOP.connection()
    isolate_ids = conn.get_isolate_ids(db_seed=3, limit=dataset_size)
 
-   if ('DEBUG' in os.environ):
+   if ('DEBUG' in os.environ and 'VERBOSE' in os.environ):
       print("isolate_ids:")
-      debug(isolate_ids)
+      for isolate_ndx in range(min(DEBUG_PEEK_SIZE, len(isolate_ids))):
+         print("\t%s" % (isolate_ids[isolate_ndx]))
 
+   start_time = time.time()
    isolate_data = conn.get_isolate_data(data=isolate_ids, limit=initialSize)
+   finish_time = time.time() - start_time
+
+   if ('DEBUG' in os.environ and 'VERBOSE' in os.environ):
+      print("isolate_data:")
+      for isolate_item in isolate_data.items():
+         print("\tisolate [%s]: %s" % (isolate_item[0], isolate_item[1]))
 
    if ('DEBUG' in os.environ):
-      print("isolate_data:")
-      debug(isolate_data)
+      print("grabbed data for %d isolates in %d minutes" %
+            (len(isolate_data.items()), finish_time / 60))
